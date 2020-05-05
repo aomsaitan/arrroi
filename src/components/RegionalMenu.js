74cde5cb0d096d881e2ec05b8f67f5ed9811da6c
Goggags123 <wongsakorngag@gmail.com>
2020-05-05 12:18:06 +0700

    Update RegionalMenu.js

--- src/components/RegionalMenu.js
+++ src/components/RegionalMenu.js
@@ -1,12 +1,13 @@
 import React, {Component} from "react";
-import {Link, withRouter} from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
+import Image from './Image'
 
 class RegionalMenu extends Component {
 	render() {
 		return (
 			<Link className="link region-link textS column-flex" to = '/menu'>
 					<div className = "region-title">{this.props.title}</div>
-					<img className = "region-img" name={this.props.nameFood} alt={this.props.alt} />
+					<Image className = "region-img" nameFood={this.props.nameFood} alt={this.props.alt} />
 					<div className = "region-more">เพิ่มเติม</div>
 			</Link>
 		);
